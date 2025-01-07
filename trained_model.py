8a15cf4dd4a8e7145529f4d84dceb9da6d132194
tgy1201 <dndsm82@kyonggi.ac.kr>
2025-01-07 17:54:18 +0900

    ai 스캔클래스 중복 제거 및 개수 반환환

--- trained_model.py
+++ trained_model.py
@@ -23,19 +23,29 @@ async def predict(request: ImageRequest):
     predictions = []
     
     for result in results:
-        plots = result.plot() # 예측된 이미지 플롯
+        plots = result.plot()  # 예측된 이미지 플롯
         
+        class_counts = {}  # 클래스별 개수를 저장할 딕셔너리
+
+        # 클래스 개수 세기
         for cls, conf in zip(result.boxes.cls, result.boxes.conf):
-            predictions.append({
-                "class": model.names[int(cls)], 
-                "reliability": round(float(conf), 2),
-            })
+            class_name = model.names[int(cls)]
+            if class_name in class_counts:
+                class_counts[class_name] += 1
+            else:
+                class_counts[class_name] = 1
+
+        # 중복 제거된 predictions 리스트 생성
+        predictions = [
+            {"class": class_name, "count": count}
+            for class_name, count in class_counts.items()
+        ]
             
     #openCV 형식으로 변환
     _, buffer = cv2.imencode('.jpg', plots)
     result_base64 = base64.b64encode(buffer).decode('utf-8')
 
-    return {"predictions": predictions, "img": "result_base64.jpg"}
+    return {"predictions": predictions, "img": result_base64}
 
 
 #실행 uvicorn trained_model:app --reload
